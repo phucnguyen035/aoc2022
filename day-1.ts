c8d7a487fec58c857480fa770dd8f4b15c63c84c
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2022-12-05 14:20:19 +0700

    feat: :sparkles: complete day 1

---
+++ day-1.ts
@@ -0,0 +1,22 @@
+const file = await Deno.readFile("input.txt");
+const input = new TextDecoder().decode(file);
+const array = input.split("\n");
+const elves = array.reduce(
+  (acc, curr) => {
+    if (curr === "") {
+      acc.push(0);
+    } else {
+      acc[acc.length - 1] += parseInt(curr);
+    }
+
+    return acc;
+  },
+  [0]
+);
+
+const sum = elves
+  .sort((a, b) => b - a)
+  .slice(0, 3)
+  .reduce((a, b) => a + b, 0);
+
+console.log("sum: ", sum);
